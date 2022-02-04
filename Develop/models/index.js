5600959120e34e34e9c331694f6e33838b38f7d3
Cole Webster <Cole.Webster@gmail.com>
2022-02-04 14:55:56 -0500

    Updating from starter example in class

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
 
+})
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tags, {
 
+})
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Products, {
+  
+})
 
 module.exports = {
   Product,
